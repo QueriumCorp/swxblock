e8bb17e3a6721a7bbe90dec493df28ece365fb34
JVR <vrracing@winning.com>
2018-09-10 14:33:08 -0500

    wrapped scrollIntoView in a setTimeout

--- swxblock/static/js/src/swxstudent.js
+++ swxblock/static/js/src/swxstudent.js
@@ -111,7 +111,10 @@ function SWXStudent(runtime, element, question) {
         stepwise_element.style.display = 'block';
         swxblock_block.classList.add("block_working");
         swxblock_block.classList.remove("block_worked");
-        swxblock_block.scrollIntoView({ behavior:"smooth"});
+        setTimeout( function(){
+            console.info("scrolling");
+            swxblock_block.scrollIntoView({ behavior:"smooth"});
+        }, 0);
         querium.startQuestion( 'OpenStaxHomework', sId, qDef, callbacks, options, stepwise_element );    
     }
 
