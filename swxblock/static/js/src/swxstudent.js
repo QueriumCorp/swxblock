088eb0151e70a2c170c036182ffde9d359a2d770
vrracing <vrracing@winning.com>
2019-01-10 08:13:44 -0600

    Removed code to get ai server assignment from dispatcher

--- swxblock/static/js/src/swxstudent.js
+++ swxblock/static/js/src/swxstudent.js
@@ -268,53 +268,6 @@ function SWXStudent(runtime, element, data) {
 
     /* PAGE LOAD EVENT */
     $(function ($) {
-        var lastUpdate = localStorage.getItem( 'oscaServerLastSet' );
-        if( 
-            (!lastUpdate) || // no server assignment update timestamp
-            (lastUpdate && ((Date.now() - lastUpdate) > 600000) ) // if lastUpdate was more than 10 minutes ago
-        ){ 
-            fetch( 'https://editorial.querium.com/cgi-bin/getserver.cgi?appId=OSCA' )
-            .then( (resp) => resp.json() )
-            .then( function( response ){
-                var newServer = response.result;
-                console.info('New server assigned:', newServer);
-                if( newServer && newServer.length ){
-                    localStorage.setItem("server", newServer);
-                    localStorage.setItem( 'oscaServerLastSet', Date.now() );
-                }
-            })
-            .catch( function (err) {
-                    console.info('Dispatcher ERRORED. Current server value is:', localStorage.getItem("server") );
-            });
-        }
-
-        function timeSince(date) {
-            if( typeof date !== 'number' ){
-                return "INVALID-DATE"
-            }
-            var seconds = Math.floor((new Date() - date) / 1000);
-            var interval = Math.floor(seconds / 31536000);
-            if (interval > 1) {
-                return interval + " years";
-            }
-            interval = Math.floor(seconds / 2592000);
-            if (interval > 1) {
-                return interval + " months";
-            }
-            interval = Math.floor(seconds / 86400);
-            if (interval > 1) {
-                return interval + " days";
-            }
-            interval = Math.floor(seconds / 3600);
-            if (interval > 1) {
-                return interval + " hours";
-            }
-            interval = Math.floor(seconds / 60);
-            if (interval > 1) {
-                return interval + " minutes";
-            }
-            return Math.floor(seconds) + " seconds";
-        }
     });
 
     // wrap element as core.js may pass a raw element or an wrapped one
