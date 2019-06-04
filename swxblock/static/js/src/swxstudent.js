86c7fd0df1a1f606ae199832b3e945821c8c7902
Kent Fuka <kent@kncl.com>
2019-06-04 16:07:56 -0600

    get enable_showme and enable_hint from the correct object

--- swxblock/static/js/src/swxstudent.js
+++ swxblock/static/js/src/swxstudent.js
@@ -4,8 +4,8 @@ function SWXStudent(runtime, element, data) {
     var question = data.question;
     var grade = data.grade;
     var solution = data.solution;
-    var enable_showme = data.q_option_showme;
-    var enable_hint = data.q_option_hint;
+    var enable_showme = question.q_option_showme;
+    var enable_hint = question.q_option_hint;
 
     console.info("enable_showme",enable_showme);
     console.info("enable_hint",enable_hint);
