22f70a3d0904e2b2643078d70d82eed412a35129
Kent Fuka <kent@kncl.com>
2019-06-23 12:54:44 -0600

    fix typo

--- swxblock/swxblock.py
+++ swxblock/swxblock.py
@@ -452,7 +452,7 @@ class SWXBlock(StudioEditableXBlockMixin, XBlock):
             "question" : question,
             "grade" :self.grade,
             "solution" : self.solution,
-            "count_attempts" : self.count_attempts;
+            "count_attempts" : self.count_attempts
         }
 
         html = self.resource_string("static/html/swxstudent.html")
