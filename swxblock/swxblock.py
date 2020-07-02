0425f17bea9c0869c24dc777a703239c310bb1ef
Kent Fuka <kent@kncl.com>
2020-07-02 15:32:10 -0600

    change variables for solution

--- swxblock/swxblock.py
+++ swxblock/swxblock.py
@@ -608,17 +608,21 @@ class SWXBlock(StudioEditableXBlockMixin, XBlock):
         valid_steps = 0;
 
 	# fuka july-2020 count the number of valid steps in the solution
-        logger.info("SWXblock save_grade() count valid_steps data={d}".format(d=data))
-        logger.info("SWXblock save_grade() count valid_steps data['solution']={s}".format(s=data['solution']))
-        logger.info("SWXblock save_grade() count valid_steps solution.stepDetails={d}".format(d=data['solution'].stepDetails))
+        solution=self.solution
+        logger.info("SWXblock save_grade() count valid_steps solution={s}".format(s=solution))
+        logger.info("SWXblock save_grade() count valid_steps solution.stepDetails={d}".format(d=solution.stepDetails))
         for c in range(data['solution'].stepDetails.length-1):
             for i in range (data['solution'].stepDetails[c].info.length-1):
                 status = data['solution'].stepDetails[c].info[i].status
-                logger.info("SWXblock save_grade() examine step c={c} i={i} status={s}".format(c=c,i=i,s=status))
-                if (status == 1):   # valid step
+                logger.info("SWXblock save_grade() count valid_steps examine step c={c} i={i} status={s}".format(c=c,i=i,s=status))
+                if (status == 0):   # victory
                     valid_steps += 1
-                # elsif (status == 3):   # invalid step
+                elif (status == 1):   # valid step
+                    valid_steps += 1
+                # elif (status == 3):   # invalid step
                 #   valid_steps += 1
+                else:
+                   logger.info("SWXblock save_grade() count valid_steps ignoring status={s}".format(s=status))
         logger.info("SWXblock save_grade() count valid_steps final valid_steps={v}".format(v=valid_steps))
 
         grade=3
