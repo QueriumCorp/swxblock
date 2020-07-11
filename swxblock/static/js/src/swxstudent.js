747cafa7dad0d4220eca11c528afe9fcc4f04131
Kent Fuka <kent@kncl.com>
2020-07-11 10:22:23 -0600

    fix bad log message, comment out start callback

--- swxblock/static/js/src/swxstudent.js
+++ swxblock/static/js/src/swxstudent.js
@@ -179,16 +179,16 @@ function SWXStudent(runtime, element, data) {
         // Callback when the student begins to work on a problem (e.g. enters a step, clicks "Hint", clicks "Show Solution"
         function start_attempt(status) {
 
-            console.info("start_attempt status={s}".format(s=status))
-            //console.info("start_attempt sessionId={s} timeMark={t} action:={a}".format(a=status.sessionId,t=status.timeMark,a=status.action))
+            console.info("start_attempt status=",status)
+            //console.info("start_attempt sessionId=",status.sessionId," timeMark=",status.timeMark," action=",a=status.action)
 
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-            $.ajax({
-                type: "POST",
-                url: handlerUrlStart,
-                data: JSON.stringify(stats),
-                success: null
-            });
+            // MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+            // $.ajax({
+                // type: "POST",
+                // url: handlerUrlStart,
+                // data: JSON.stringify(stats),
+                // success: null
+            // });
         }
 
         var callbacks = {
