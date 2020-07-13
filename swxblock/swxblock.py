01df5f2a6b56ef535dc6d437cf04b3f05e6e7a6e
Kent Fuka <kent@kncl.com>
2020-07-13 13:58:12 -0600

    typo

--- swxblock/swxblock.py
+++ swxblock/swxblock.py
@@ -1085,7 +1085,6 @@ class SWXBlock(StudioEditableXBlockMixin, XBlock):
         logger.info("SWXblock start_attempt() entered")
         logger.info("SWXBlock start_attempt() self.count_attempts={c} max_attempts={m} len(self.variants_attempted)={l}".format(c=self.count_attempts,m=self.max_attempts,l=len(self.variants_attempted)))
         logger.info("SWXBlock start_attempt() len(self.variants_attempted)={l} self.variants_attempted={v}".format(l=len(self.variants_attempted),v=self.variants_attempted))
-        //logger.info("SWXBlock start_attempt() data={d}".format(d=data))
         logger.info("SWXBlock start_attempt() action={d} sessionId={s} timeMark={t}".format(d=data['status']['action'],s=data['status']['sessionId'],t=data['status']['timeMark']))
         self.count_attempts += 1
         if data['q_index'] in self.variants_attempted:
