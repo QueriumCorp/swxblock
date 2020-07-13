14c5e90fe4d5428a01022ab5b8452aa86ad799a0
Kent Fuka <kent@kncl.com>
2020-07-13 14:49:37 -0600

    uncomment logging

--- swxblock/swxblock.py
+++ swxblock/swxblock.py
@@ -1087,13 +1087,15 @@ class SWXBlock(StudioEditableXBlockMixin, XBlock):
         logger.info("SWXBlock start_attempt() self.count_attempts={c} max_attempts={m}".format(c=self.count_attempts,m=self.max_attempts))
         logger.info("SWXBlock start_attempt() len(self.variants_attempted)={l} self.variants_attempted={v}".format(l=len(self.variants_attempted),v=self.variants_attempted))
         logger.info("SWXBlock start_attempt() action={d} sessionId={s} timeMark={t}".format(d=data['status']['action'],s=data['status']['sessionId'],t=data['status']['timeMark']))
-        # logger.info("SWXBlock start_attempt() passed q_index={q}".format(q=data['q_index']))
-        # self.count_attempts += 1
-        # if data['q_index'] in self.variants_attempted:
-        #     logger.info("variant {v} has already been attempted!".format(v=data['q_index']))
-        # else:
-        #     self.variants_attempted.add(data['q_index'])
-        #     logger.info("adding variant {v} to self.variants_attempted len={l}".format(v=data['q_index'],l=len(self.variants_attempted)))
+        logger.info("SWXBlock start_attempt() passed q_index={q}".format(q=data['q_index']))
+        ###
+        self.count_attempts += 1
+        if data['q_index'] in self.variants_attempted:
+            logger.info("variant {v} has already been attempted!".format(v=data['q_index']))
+        else:
+            self.variants_attempted.add(data['q_index'])
+            logger.info("adding variant {v} to self.variants_attempted len={l}".format(v=data['q_index'],l=len(self.variants_attempted)))
+        ###
         # logger.info("SWXBlock start_attempt() updated self.count_attempts={c}".format(c=self.count_attempts))
         # logger.info("SWXBlock start_attempt() updated len(self.variants_attempted)={l} self.variants_attempted={v}".format(l=len(self.variants_attempted),v=self.variants_attempted))
         logger.info("SWXBlock start_attempt() done")
