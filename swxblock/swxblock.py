9b5f9c431648355dff07fa1a49efbc0e94588ed6
Kent Fuka <kent@kncl.com>
2020-07-21 17:55:28 -0600

    convert due date to epoch seconds

--- swxblock/swxblock.py
+++ swxblock/swxblock.py
@@ -640,7 +640,7 @@ class SWXBlock(StudioEditableXBlockMixin, XBlock):
             "solution" : self.solution,
             "count_attempts" : self.count_attempts,
             "variants_count" : self.variants_count,
-            "due" : self.due
+            "due" : (self.due - datetime.datetime(1970,1,1)).total_seconds()	# Convert to epoch time
         }
 
         html = self.resource_string("static/html/swxstudent.html")
